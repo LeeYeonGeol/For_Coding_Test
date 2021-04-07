e99ede5469e9cfc8bf884085f8107e21f478f6c5
LeeYeonGeol <48538655+LeeYeonGeol@users.noreply.github.com>
2021-04-07 17:49:22 +0900

    Create Sol_14501번_퇴사.py

---
+++ BOJ/Sol_14501번_퇴사.py
@@ -0,0 +1,25 @@
+n = int(input())
+#n = 7
+
+arr = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    arr.append((a, b))
+
+
+ans = 0
+
+def dfs(time, csum):
+    global ans
+    if time > n:
+        return
+    ans = max(ans, csum)
+
+    for i in range(time, n):
+        dfs(i+arr[i][0], csum+arr[i][1])
+
+
+for i in range(n):
+    dfs(i, 0)
+
+print(ans)
