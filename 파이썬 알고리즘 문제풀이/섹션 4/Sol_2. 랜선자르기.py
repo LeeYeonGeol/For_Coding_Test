076ee548da19e8d05da564b9a25943303c187cf2
LeeYeonGeol <48538655+LeeYeonGeol@users.noreply.github.com>
2021-04-19 12:35:23 +0900

    Update Sol_2. 랜선자르기.py

--- 파이썬 알고리즘 문제풀이/섹션 4/Sol_2. 랜선자르기.py
+++ 파이썬 알고리즘 문제풀이/섹션 4/Sol_2. 랜선자르기.py
@@ -4,30 +4,19 @@ arr = []
 for _ in range(k):
     arr.append(int(input()))
 
-left = 0
+left = 1
 right = max(arr)
-
-result = 0
-
+ans = 0
 while(left <= right):
     mid = (left+right) // 2
-    result = mid
     cnt = 0
-    remain = 0
     for a in arr:
-        cnt += (a // mid)
-        remain += (a % mid)
+        cnt += a // mid
+
     if cnt < n:
         right = mid - 1
-    elif cnt > n:
-        left = mid + 1
     else:
-        if remain <= 0:
-            break
-        else:
-            left += 1
+        ans = mid
+        left = mid + 1
 
-
-print(result)
-
-
+print(ans)
