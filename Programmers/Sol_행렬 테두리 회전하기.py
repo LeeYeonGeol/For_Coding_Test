2c0c3fb17c879efb0f6a633c1f2eb4c56e773ebd
LeeYeonGeol <f2f42012@gmail.com>
2022-03-12 00:35:14 +0900

    Update Sol_행렬 테두리 회전하기.py

--- Programmers/Sol_행렬 테두리 회전하기.py
+++ Programmers/Sol_행렬 테두리 회전하기.py
@@ -1,36 +1,45 @@
+from collections import defaultdict
 def solution(rows, columns, queries):
     answer = []
-
     graph = [[0]*columns for _ in range(rows)]
     cnt = 1
     for i in range(rows):
         for j in range(columns):
             graph[i][j] = cnt
             cnt += 1
-    for query in queries:
-        x1, y1, x2, y2 = query
-        n1, n2 = x2-x1, y2-y1
-        x, y = x1-1, y1-1
-        nx, ny = 0, 0
 
-        value = int(1e9)
-        for _ in range(n1):
-            graph[x][y+1], graph[x][y] = graph[x][y], graph[x][y+1]
+    for a1, b1, a2, b2 in queries:
+
+        db = defaultdict(list)
+        x1, y1 = a1-1, b1-1
+        x2, y2 = a2-1, b2-1
+        x, y = x1, y1
+        sub_answer = 1e9
+        # 가로 (위)
+        for _ in range(y2-y1):
+            db[graph[x][y]] += [x,y+1]
+            sub_answer = min(sub_answer, graph[x][y])
             y += 1
 
-        for _ in range(n2):
-            graph[x+1][y], graph[x][y] = graph[x][y], graph[x+1][y]
+        # 세로 (오른)
+        for _ in range(x2-x1):
+            db[graph[x][y]] += [x+1,y]
+            sub_answer = min(sub_answer, graph[x][y])
             x += 1
 
-
-        for _ in range(n1):
-            graph[x][y-1], graph[x][y] = graph[x][y], graph[x][y-1]
+        # 가로 (아래)
+        for _ in range(y2-y1):
+            db[graph[x][y]] += [x,y-1]
+            sub_answer = min(sub_answer, graph[x][y])
             y -= 1
 
-        for _ in range(n2):
-            graph[x-1][y], graph[x][y] = graph[x][y], graph[x-1][y]
+        # 세로 (왼)
+        for _ in range(x2-x1):
+            db[graph[x][y]] += [x-1,y]
+            sub_answer = min(sub_answer, graph[x][y])
             x -= 1
-
-        answer.append(value)
-
+        for value in db:
+            nx, ny = db[value]
+            graph[nx][ny] = value
+        answer.append(sub_answer)
     return answer
